889f53e844fa61a4b469939a369f90b0aee131eb
Chohs <evergreen1286@gmail.com>
2022-12-07 13:42:22 +0900

    Remove comments

--- src/edu/kh/variable/ex2/ScannerExample.java
+++ src/edu/kh/variable/ex2/ScannerExample.java
@@ -7,18 +7,18 @@ public class ScannerExample {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-//		System.out.print("값을 입력하시오: ");
+		System.out.print("값을 입력하시오: ");
 		
-//		int input1 = sc.nextInt(); 
-//		System.out.println(input1);
+		int input1 = sc.nextInt(); 
+		System.out.println(input1);
 		
-//		System.out.print("실수 값을 입력하시오: ");
-//		
-//		double input2 = sc.nextDouble();
-//		System.out.print(input2);
+		System.out.print("실수 값을 입력하시오: ");
+		
+		double input2 = sc.nextDouble();
+		System.out.print(input2);
 		
 		System.out.print("입력1: ");
-		String input3 = sc.next(); // 다음 입력된 '한 단어'를 읽어옴
+		String input3 = sc.next();
 		System.out.println(input3);
 		
 		System.out.print("입력2: ");

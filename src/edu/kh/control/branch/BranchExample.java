f08b42da2ca24de1d02a5b6f386f92c4b54c48e6
Chohs <evergreen1286@gmail.com>
2022-12-09 15:19:00 +0900

    For, While, Continue, Break Practice

---
+++ src/edu/kh/control/branch/BranchExample.java
@@ -0,0 +1,73 @@
+package edu.kh.control.branch;
+
+import java.util.Scanner;
+
+public class BranchExample {
+	public void ex1() {
+		// 1부터 10까지 1씩 증가하며 출력하는 반복문 작성 (for문 사용)
+		// 단, 5를 출력하면 반복문을 멈춤 (if)
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i);
+			
+			if(i == 5) {
+				break;
+			}
+		}
+	}
+
+	public void ex2() {
+		// 0이 입력될 때 까지 모든 정수 합 구하기
+		Scanner sc = new Scanner(System.in);
+		
+		int input = 0;
+		int sum = 0;
+		
+		while(true) {
+			System.out.print("정수입력: ");
+			input = sc.nextInt();
+			
+			if(input == 0) {
+				break;
+			}
+			sum += input;
+		}
+		
+		System.out.println("합계: " + sum);
+	}
+
+	public void ex3() {
+		// break: 반복문을 바로 멈춤
+		// continue: 다음 반복으로 넘어감
+		
+		// 3의 배수 제외하고 1~10까지 출력
+		for(int i = 1; i <= 10; i++) {
+			if(i % 3 == 0) {
+				continue;
+			}
+			System.out.print(i + " ");
+		}
+	}
+
+	public void ex4() {
+		// 1~100까지 1씩 증가하며 출력하는 반복문
+		// 단, 5의 배수는 건너뛰고, 증가하는 값이 40이 되었을 때 반복 멈춤
+		
+		for(int i = 1; i <= 100; i++) {
+			
+//			if(i % 5 == 0) {
+//				continue;
+//			}else if(i >= 40) { // 40 == 0 으로 하면, continue 조건에서 넘어가서 이 부분을 만나지 않음
+//				break;
+//			}
+			
+			if(i == 40) {
+				break;
+			}else if(i % 5 == 0) {
+				continue;
+			}
+			
+			System.out.println(i);
+		}
+	}
+}

8e423906848ab1bb7f138e89df0499e8a66b3107
Chohs <evergreen1286@gmail.com>
2022-12-16 15:15:16 +0900

    Practice OOP examples

---
+++ src/com/kh/test/money/model/vo/Money.java
@@ -0,0 +1,26 @@
+package com.kh.test.money.model.vo;
+
+public class Money {
+	public static final String UNIT = "원";
+	private int money;
+	
+	public Money() {}
+	
+	public Money(int money){
+		this.money = money;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	
+	public void print() {
+		System.out.println(money + UNIT);
+	}
+	
+	
+}

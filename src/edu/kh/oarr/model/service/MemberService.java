635fe310add75946de2f0f0f0bf59a396908ffb4
Hyeongshin <evergreen1286@gmail.com>
2022-12-19 23:27:20 +0900

    Create login(), selectMember(), updateMember() methods

--- src/edu/kh/oarr/model/service/MemberService.java
+++ src/edu/kh/oarr/model/service/MemberService.java
@@ -28,8 +28,8 @@ public class MemberService {
 			System.out.println("\n ==== 회원 정보 관리 프로그램 v2 ====");
 			System.out.println("1. 회원가입");
 			System.out.println("2. 로그인");
-			System.out.println("3. 회원 정보 조회"); // 과제
-			System.out.println("4. 회원 정보 수정"); // 선택 과제
+			System.out.println("3. 회원 정보 조회"); 
+			System.out.println("4. 회원 정보 수정");
 			System.out.println("5. 회원 검색(지역)"); // 과제 X
 			System.out.println("0. 프로그램 종료");
 			
@@ -38,12 +38,23 @@ public class MemberService {
 			sc.nextLine();
 			
 			switch(menuNum){
-			case 1: break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
+			case 1: System.out.println( signUp() ); break;
+			case 2: System.out.println( login() ); break;
+			case 3: System.out.println( selectMember() ); break;
+			case 4: 
+				int result = updateMember();
+				
+				if(result == -1) {
+					System.out.println("로그인 해주시기 바랍니다");
+				}else if(result == 1) {
+					System.out.println("회원 정보 수정이 완료되었습니다.");
+				}else {
+					System.out.println("회원 정보 수정 실패 (비밀번호 불일치)");
+				}
+				
+				break;
 			case 5: break;
-			case 0: break;
+			case 0: System.out.println("프로그램을 종료합니다."); break;
 			default : System.out.println("\n잘못 입력하셨습니다."); 
 			}
 			
@@ -113,14 +124,85 @@ public class MemberService {
 	} 
 	
 	public String login() {
+		System.out.println("======== 로그인 ========");
 		// 1) memberArr 배열 내 요소를 순서대로 접근하여 null이 아닌지 확인
-			// 회원정보가 있을 경우 
-			// 2) 회원정보(memberArr[i])의 아이디, 비밀번호와
-			// 입력받은 아이디, 비밀번호가 같은지 확인
-				// 3) 로그인 회원 정보 객체(Member)를 참조하는 변수 loginMember에
-				// 	  현재 접근중인 memberArr[i]요소에 저장된 주소를 얕은 복사
+		System.out.print("아이디 : ");
+		String memberId = sc.next();
 		
+		System.out.print("비밀번호 : ");
+		String memberPw = sc.next();
+		
+		for(int i = 0; i < memberArr.length; i++) {
+			if(memberArr[i] != null) {
+				// 회원정보가 있을 경우 
+				// 2) 회원정보(memberArr[i])의 아이디, 비밀번호와
+				// 입력받은 아이디, 비밀번호가 같은지 확인
+				
+				if(memberArr[i].getMemberId().equals(memberId) 
+					&& memberArr[i].getMemberPw().equals(memberPw)) {
+					// 3) 로그인 회원 정보 객체(Member)를 참조하는 변수 loginMember에
+					// 	  현재 접근중인 memberArr[i]요소에 저장된 주소를 얕은 복사
+					loginMember = memberArr[i];
+					return loginMember.getMemberName() + "님 로그인 성공!";
+				}
+			}
+		}
 		// 4) 로그인 성공/실패 여부에 따라 결과값을 반환
-		return "";
+		return "아이디 또는 비밀번호가 일치하지 않습니다.";
+	}
+
+	public String selectMember() {
+		System.out.println("======== 회원 정보 조회 ========");
+		// 1) 로그인된 멤버인지 확인 후 아닐경우 "로그인 해주시기 바랍니다" 출력
+		if(loginMember == null) {
+			return "로그인 해주시기 바랍니다";
+		}else {
+		// 2) 로그인된 멤버일 경우 해당 loginMember 인스턴스에서
+		//		getter를 이용해 정보 가져오기
+		// - 비밀번호의 경우 출력하지 않음
+		// -- 이름, 나이, 지역만 가져오기
+			
+			System.out.println("이름 : " + loginMember.getMemberName()
+							+ "\n나이 : " + loginMember.getMemberAge()
+							+ "\n지역 : " + loginMember.getRegion());
+			
+			return "회원 정보 출력 완료!";
+		}
+	}
+
+	public int updateMember() {
+		System.out.println("======== 회원 정보 수정 ========");
+		
+		// 1) 로그인 여부 체크
+		// 로그인 되지 않은 경우 -1 반환
+		if(loginMember == null) {
+			return -1;
+		}else {
+			// 2) 로그인이 되어있다면, 수정할 정보 입력받은 후 저장
+			System.out.print("수정할 이름: ");
+			String inputName = sc.next();
+			
+			System.out.print("수정할 나이: ");
+			int inputAge = sc.nextInt();
+			
+			System.out.print("수정할 지역: ");
+			String inputRegion = sc.next();
+			
+			// 현재 저장된 비밀번호와 입력받는 비밀번호가 일치할 경우 수정된 결과 저장
+			System.out.print("현재 비밀번호를 입력해주세요 : ");
+			String memberPw = sc.next(); 
+			
+			if(loginMember.getMemberPw().equals(memberPw)) {
+				loginMember.setMemberName(inputName);
+				loginMember.setMemberAge(inputAge);
+				loginMember.setRegion(inputRegion);
+				
+				// 입력받은 비밀번호가 현재 비밀 번호와 일치하는 경우 1 반환
+				return 1;
+			}else {
+				// 일치하지 않는 경우 0 반환
+				return 0;
+			}
+		}
 	}
 }

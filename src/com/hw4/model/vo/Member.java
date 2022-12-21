1d069755acd034420ebd98cdc8995f75c5d74c03
Chohs <evergreen1286@gmail.com>
2022-12-21 15:02:45 +0900

    Practice object array

---
+++ src/com/hw4/model/vo/Member.java
@@ -0,0 +1,27 @@
+package com.hw4.model.vo;
+
+public class Member {
+
+	private String userId;
+	private String userPwd;
+	private String name;
+	private int age;
+	private char gender;
+	private String email;
+	
+	public Member() {}
+
+	public Member(String userId, String userPwd, String name, int age, char gender, String email) {
+		super();
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.email = email;
+	}
+	
+	public String information() {
+		return "";
+	}
+}
---
+++ src/com/hw4/view/MemberMenu.java
@@ -0,0 +1,29 @@
+package com.hw4.view;
+
+import java.util.Scanner;
+
+import com.hw4.controller.MemberController;
+
+public class MemberMenu {
+	private MemberController mc = new MemberController();
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public void mainMenu() {
+		System.out.println("===== 회원 관리 메뉴 =====");
+		
+		
+	}
+	
+	public void insertMember() {}
+	
+	public void searchMember() {}
+	
+	public void updateMember() {}
+	
+	public void deleteMember() {}
+	
+	public void printAllMember() {}
+	
+	public void sortMember() {}
+}
